1398e8bf48aaad59031959f1a078f7420082d648
Patricia Louise Ramos <ramospatricialouise@gmail.com>
2023-10-20 22:10:18 +0800

    implement queue and stack

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,53 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
+    // Term used when adding a queue
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    // Remove from the queue
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        // Free up
+        head.next = undefined;
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
